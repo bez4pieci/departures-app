7fe4af0c49dad9f671beef22d76047969a593ba0
bez4pieci <ernests@golayer.io>
2025-05-13 18:21:41 +0200

    Set up nativewind and font

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import { useFonts } from 'expo-font';
+import { Platform, Text, View } from "react-native";
 
 export default function Index() {
+  if (Platform.OS === 'web') {
+    const [fontsLoaded] = useFonts({
+      'DepartureMono-Regular': require('../assets/fonts/DepartureMono-Regular.otf'),
+    });
+    if (!fontsLoaded) {
+      return null;
+    }
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-red-500 font-departure-mono text-2xl">...</Text>
     </View>
   );
 }
