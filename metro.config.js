4f2f63ada646efbb0bd4ea6853189d9827c79ed4
bez4pieci <ernests@golayer.io>
2025-05-18 14:51:20 +0200

    Update deps

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,7 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 const { mergeConfig } = require("metro-config");
 
-const config = mergeConfig(getDefaultConfig(__dirname), {
-  watchFolders: [require("node:path").resolve(__dirname, "..")],
-  resolver: {},
-});
+const config = mergeConfig(getDefaultConfig(__dirname), {});
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
